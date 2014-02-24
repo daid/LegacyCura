ec76b0b3611416caab3d99bbf0e954da97b8b368
nallath <nallath@gmail.com>
2014-02-24 11:37:11 +0100

    added initialization to view3d

--- Cura/gui/view3D/view3D.py
+++ Cura/gui/view3D/view3D.py
@@ -1,4 +1,7 @@
 __author__ = 'Jaime van Kessel'
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import numpy
 
 class View3D(object):
 	def __init__(self):
@@ -24,3 +27,38 @@ class View3D(object):
 
 	def getMachine(self):
 		return self._machine
+
+	def _init3DView(self):
+		'''
+		Setup the basics of the 3D view
+		'''
+		#TODO: hardcoded values for height & width
+		view_port_width = 10;
+		view_port_height = 10;
+		#size = self.GetSize() #get size of view. TODO
+		glViewport(0, 0, view_port_width, view_port_height)
+		glLoadIdentity()
+
+		glLightfv(GL_LIGHT0, GL_POSITION, [0.2, 0.2, 1.0, 0.0])
+
+		glDisable(GL_RESCALE_NORMAL)
+		glDisable(GL_LIGHTING)
+		glDisable(GL_LIGHT0)
+		glEnable(GL_DEPTH_TEST)
+		glDisable(GL_CULL_FACE)
+		glDisable(GL_BLEND)
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
+		glClearColor(0.8, 0.8, 0.8, 1.0)
+		glClearStencil(0)
+		glClearDepth(1.0)
+
+		glMatrixMode(GL_PROJECTION)
+		glLoadIdentity()
+		aspect = float(view_port_width) / float(view_port_height)
+		machine_size = [self._machine.getValueByName('machine_width'),self._machine.getValueByName('machine_height'),self._machine.getValueByName('machine_depth')]
+		gluPerspective(45.0, aspect, 1.0, numpy.max(machine_size) * 4)
+
+		glMatrixMode(GL_MODELVIEW)
+		glLoadIdentity()
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT)
