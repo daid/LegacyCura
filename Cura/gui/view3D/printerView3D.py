d88ad7420f56ac5ca3c375f5a239d3f73606902d
nallath <nallath@gmail.com>
2014-02-24 11:26:32 +0100

    added render function to view3D

--- Cura/gui/view3D/printerView3D.py
+++ Cura/gui/view3D/printerView3D.py
@@ -1,10 +1,14 @@
 __author__ = 'Jaime van Kessel'
 from Cura.gui.view3D.view3D import View3D
 from Cura.gui.view3D.machineRenderer import MachineRenderer
+from Cura.gui.view3D.modelRenderer import ModelRenderer
 
 class PrinterView3D(View3D):
 	def __init__(self):
 		super(PrinterView3D,self).__init__()
+
 		machine_renderer = MachineRenderer()
 		machine_renderer.setMachine(self._machine)
 		self.addRenderer(machine_renderer)
+		model_renderer = ModelRenderer()
+		self.addRenderer(model_renderer)
--- Cura/gui/view3D/view3D.py
+++ Cura/gui/view3D/view3D.py
@@ -6,6 +6,10 @@ class View3D(object):
 		self._renderer_list = [] #The view holds a set of renderers, such as machine renderer or object renderer.
 		self._machine = None # Reference to the machine
 
+	def render(self): #todo: Unsure about name.
+		for renderer in self._renderer_list:
+			renderer.render() #call all render functions
+
 	def addRenderer(self, renderer):
 		self._renderer_list.append(renderer);
 
